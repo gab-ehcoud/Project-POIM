6e38b86eac73176604c63848297e3cde941b3fb8
utkarshdevlab <48464878+utkarshdevlab@users.noreply.github.com>
2020-08-01 19:50:32 +0530

    Update and rename payments.html to payments.php

--- payments.php
+++ payments.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>POIM1</title>
+    <title>POIM</title>
     <meta name="theme-color" content="#00a80a">
     <link rel="icon" type="image/png" sizes="undefinedxundefined" href="assets/img/BeFunky-design%20(45).png">
     <link rel="icon" type="image/png" sizes="undefinedxundefined" href="assets/img/BeFunky-design%20(45).png">
