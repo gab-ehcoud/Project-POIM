4694323863ea4d1b27754516da2ae157cbb328b3
Gaurav Dhiman <gdhiman235@gmail.com>
2020-08-02 21:37:22 +0530

    Delete messages.php

--- messages.php
+++
@@ -1,7 +0,0 @@
-<?php  if (count($errors) > 0) : ?>
-  <div class="msg">
-  	<?php foreach ($errors as $error) : ?>
-  	  <span><?php echo $error ?></span>
-  	<?php endforeach ?>
-  </div>
-<?php  endif ?>
