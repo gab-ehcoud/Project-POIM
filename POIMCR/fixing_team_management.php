89f9ef96a4dd50b7829ee76dfcbecf511330bebb
utkarshdevlab <48464878+utkarshdevlab@users.noreply.github.com>
2020-08-02 19:30:38 +0530

    Update and rename fixing_team_management.html to fixing_team_management.php

--- POIMCR/fixing_team_management.php
+++ POIMCR/fixing_team_management.php
@@ -1,3 +1,24 @@
+<?php include('server.php'); ?>
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['Agent_Id'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['Agent_Id']);
+    header("location: login.php");
+  }
+$query11 = "SELECT count(*) as total_team FROM power_outage po INNER JOIN fixing_team ft WHERE po.Ft_Id = ft.Ft_Id ";
+$result11 = mysqli_query($db, $query11);
+$active_team = mysqli_fetch_assoc($result11);
+
+// Set the new timezone
+date_default_timezone_set('Asia/Kolkata');
+$date = date('d-m-y h:i:s');
+?>
 <!DOCTYPE html>
 <html>
 
@@ -10,23 +31,17 @@
     <link rel="icon" type="image/png" sizes="500x500" href="assets/img/BeFunky-design%20(45).png">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="manifest" href="manifest.json">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
     <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
     <link rel="stylesheet" href="assets/fonts/material-icons.min.css">
     <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
-    <link rel="stylesheet" href="assets/css/Article-Clean.css">
     <link rel="stylesheet" href="assets/css/Bottom-Resonsive-Menu.css">
     <link rel="stylesheet" href="assets/css/Contact-Form-Clean.css">
     <link rel="stylesheet" href="assets/css/Footer-Basic.css">
     <link rel="stylesheet" href="assets/css/Highlight-Blue.css">
-    <link rel="stylesheet" href="assets/css/Highlight-Clean.css">
-    <link rel="stylesheet" href="assets/css/Highlight-Phone.css">
     <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
-    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
     <link rel="stylesheet" href="assets/css/styles.css">
-    <link rel="stylesheet" href="assets/css/Team-Boxed.css">
 </head>
 
 <body><iframe allowfullscreen="" frameborder="0" src="https://cdn.bootstrapstudio.io/placeholders/map.html" width="100%" height="400"></iframe>
@@ -37,7 +52,7 @@
                     <div class="card">
                         <div class="card-body" style="background-color: #ff6363;">
                             <h6 class="text-center card-title">Active Team</h6>
-                            <h5 class="text-center" style="color: rgb(255,255,255);">&lt;count(*)&gt;</h5>
+                            <h5 class="text-center" style="color: rgb(255,255,255);"><?php echo $active_team["total_team"];?></h5>
                         </div>
                     </div>
                 </div>
@@ -45,6 +60,7 @@
                     <div class="card">
                         <div class="card-body" style="background-color: #fff742;">
                             <h6 class="text-center card-title">SOS Request</h6>
+				<h5 class="text-center" style="color: rgb(255,255,255);">N/A</h5>
                         </div>
                     </div>
                 </div>
@@ -52,6 +68,7 @@
                     <div class="card">
                         <div class="card-body" style="background-color: #637cff;">
                             <h6 class="text-center card-title">Inventory Req</h6>
+				<h5 class="text-center" style="color: rgb(255,255,255);">N/A</h5>
                         </div>
                     </div>
                 </div>
