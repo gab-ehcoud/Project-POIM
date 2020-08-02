d4600b3b2691437caea0b9adaa236358bc757908
utkarshdevlab <48464878+utkarshdevlab@users.noreply.github.com>
2020-08-02 21:37:02 +0530

    login with backend connected

---
+++ POIM Fixing Team/login.php
@@ -0,0 +1,33 @@
+<?php include('server.php'); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>FixingTeam</title>
+    <link rel="icon" type="image/png" sizes="undefinedxundefined" href="assets/img/BeFunky-design%20(45).png">
+    <link rel="icon" type="image/png" sizes="undefinedxundefined" href="assets/img/BeFunky-design%20(45).png">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="manifest" href="manifest.json">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/Bottom-Resonsive-Menu.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div class="login-dark" style="height: 696px;">
+        <form method="post">
+            <h2 class="sr-only">Login Form</h2>
+            <h1 class="text-center" style="font-size: 26px;color: rgb(33,74,128);">Fixing Team</h1>
+            <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
+            <div class="form-group"><input class="form-control" type="text" name="Ft_Id" placeholder="Fixing Team ID"></div>
+            <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
+            <div class="form-group"><button class="btn btn-primary btn-block" type="submit" name="fix_team">Log In</button></div><a class="forgot" href="#">Forgot your email or password?</a></form>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
