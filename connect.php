ca0ed7b984bd637ac796d8c7eacb42e486678cdf
Darshan <51724336+ItsKDM@users.noreply.github.com>
2020-08-02 21:29:39 +0530

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db = mysqli_connect('sql106.epizy.com', 'epiz_26406772', 'b2XAyYwf8gbzhpa', 'epiz_26406772_poim');
-
-// Check connection
-if ($db -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-?>
