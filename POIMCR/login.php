0d84a079929cb9d5ad7d23e9927f1b02676bbebe
utkarshdevlab <48464878+utkarshdevlab@users.noreply.github.com>
2020-08-02 14:45:10 +0530

    Update and rename login.html to login.php

--- POIMCR/login.php
+++ POIMCR/login.php
@@ -1,3 +1,4 @@
+<?php include('server.php'); ?>
 <!DOCTYPE html>
 <html>
 
@@ -10,36 +11,29 @@
     <link rel="icon" type="image/png" sizes="500x500" href="assets/img/BeFunky-design%20(45).png">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="manifest" href="manifest.json">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
     <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
     <link rel="stylesheet" href="assets/fonts/material-icons.min.css">
     <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
-    <link rel="stylesheet" href="assets/css/Article-Clean.css">
     <link rel="stylesheet" href="assets/css/Bottom-Resonsive-Menu.css">
     <link rel="stylesheet" href="assets/css/Contact-Form-Clean.css">
     <link rel="stylesheet" href="assets/css/Footer-Basic.css">
     <link rel="stylesheet" href="assets/css/Highlight-Blue.css">
-    <link rel="stylesheet" href="assets/css/Highlight-Clean.css">
-    <link rel="stylesheet" href="assets/css/Highlight-Phone.css">
     <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
-    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
     <link rel="stylesheet" href="assets/css/styles.css">
-    <link rel="stylesheet" href="assets/css/Team-Boxed.css">
 </head>
 
 <body>
-    <div class="bg-success login-dark" style="background-color: rgb(3,182,222);height: 660px;min-height: 400px;max-height: 2500px;width: 100%;background-image: url(&quot;assets/img/star-sky.jpg&quot;);">
-        <form method="post">
+    <div class="bg-success login-dark" style="background-color: rgb(3,182,222);height: 667px;min-height: 400px;max-height: 2500px;width: 100%;background-image: url(&quot;assets/img/star-sky.jpg&quot;);">
+        <form method="post" action="login.php">
+		<?php include('errors.php'); ?>
             <h2 class="sr-only">Login Form</h2>
             <h4 class="text-center" style="color: rgb(33,74,128);">Control room Access</h4>
             <div class="illustration" style="padding: 0;"><i class="icon ion-ios-locked-outline"></i></div>
-            <div class="form-group"><input class="form-control" type="email" name="agent_id" placeholder="AGENT ID"></div>
+            <div class="form-group"><input class="form-control" type="text" name="agent_id" placeholder="AGENT ID"></div>
             <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
-            <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Log In</button></div><a class="forgot" href="#">Forgot your email or password?</a></form>
-        <p>Paragraph</p>
-        <p>Paragraph</p>
+            <div class="form-group"><button class="btn btn-primary btn-block" type="submit" name="login_agent">Log In</button></div><a class="forgot" href="reset_pass.html">Forgot your email or password?</a></form>
     </div>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
