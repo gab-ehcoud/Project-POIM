3cf2bf6de84f74e803a3788c80acafbc40732d79
utkarshdevlab <48464878+utkarshdevlab@users.noreply.github.com>
2020-08-02 19:31:29 +0530

    Update and rename outage_manual.html to outage_manual.php

--- POIMCR/outage_manual.php
+++ POIMCR/outage_manual.php
@@ -1,4 +1,18 @@
 <!DOCTYPE html>
+<?php include('server.php'); ?>
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['Agent_Id'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['Agent_Id']);
+    header("location: login.php");
+  }
+?>
 <html>
 
 <head>
@@ -10,13 +24,11 @@
     <link rel="icon" type="image/png" sizes="500x500" href="assets/img/BeFunky-design%20(45).png">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="manifest" href="manifest.json">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
     <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
     <link rel="stylesheet" href="assets/fonts/material-icons.min.css">
     <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
-    <link rel="stylesheet" href="assets/css/Article-Clean.css">
     <link rel="stylesheet" href="assets/css/Bottom-Resonsive-Menu.css">
     <link rel="stylesheet" href="assets/css/Contact-Form-Clean.css">
     <link rel="stylesheet" href="assets/css/Footer-Basic.css">
@@ -32,13 +44,13 @@
 <body>
     <div class="register-photo">
         <div class="form-container">
-            <form method="post">
+            <form method="post" action="outage_manual.php">
                 <h2 class="text-center"><strong>Report Manually</strong></h2>
-                <div class="form-group"><input class="form-control" type="email" name="ac" placeholder="Area_Code"></div>
-                <div class="form-group"><input class="form-control" type="email" name="pt" placeholder="Problem Title"></div>
-                <div class="form-group"><input class="form-control" type="email" name="et" placeholder="Est_Time"></div>
-                <div class="form-group"><input class="form-control" type="email" name="mno" placeholder="Module_No"></div>
-                <div class="form-group"><button class="btn btn-primary btn-block" type="submit" style="background-color: #00a80a;">Report</button></div>
+                <div class="form-group"><input class="form-control" type="text" name="ac" placeholder="Area_Code" ></div>
+                <div class="form-group"><input class="form-control" type="text" name="pt" placeholder="Problem Title" ></div>
+                <div class="form-group"><input class="form-control" type="text" name="et" placeholder="Est_Time" ></div>
+                <div class="form-group"><input class="form-control" type="text" name="mno" placeholder="Module_No"></div>
+                <div class="form-group"><button class="btn btn-primary btn-block" type="submit" name="manual_power" style="background-color: #00a80a;">Report</button></div>
             </form>
         </div>
     </div>
